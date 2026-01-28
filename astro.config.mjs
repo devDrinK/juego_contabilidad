988ebab739a72cf51b71dbd8657dde10fc56f4fb
devin <devDrink@hotmail.com>
2026-01-27 22:10:12 -0400

    correcciones para github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://devDrink.github.io',
+    site: 'https://devDrinK.github.io',
     base: '/juego_contabilidad',
 });
